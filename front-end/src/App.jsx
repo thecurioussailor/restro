b2bd80752a8693a0017de87afa06a16e91d88f24
Ashutosh Sagar <48312681+thecurioussailor@users.noreply.github.com>
2024-05-06 17:10:59 +0530

    first commit

---
+++ front-end/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
+function App() {
+
+  return (
+    <div>
+          <div class="container py-4 px-3 mx-auto">
+          <h1>Hello, Bootstrap and Vite!</h1>
+          <button className="btn btn-primary">Primary button</button>
+          
+        </div>
+    </div>
+  )
+}
+
+export default App
