e0bd6198ae4d6f1fe0f82434fde4c7ddbaf5c189
Ashutosh Sagar <48312681+thecurioussailor@users.noreply.github.com>
2024-06-24 23:44:34 +0530

    restro nav

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,5 +1,4 @@
 import './App.css'
-import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
 
 import Home from "./pages/Home" // this way you can import any components
 function App() {
--- front-end/src/components/Navbar.jsx
+++ front-end/src/components/Navbar.jsx
@@ -1,31 +1,44 @@
+import Logo from "../assets/logo-main.png"
 function Navbar() {
 
     return(
         <div>
-            <nav className="d-flex justify-content-start w-100 bg-dark text-white position-fixed z-3 px-5">
-                <a className="mr-3 w-50">
-                    <h1>
-                        <ion-icon name="fast-food-outline"></ion-icon>
-                        Restro
-                    </h1>
-                </a>
-                <div className="d-flex  w-100 mr-5 align-items-center">
-                    <div className="d-flex flex-row ml-auto">
-                        <a>HOME</a>
-                        <a>ABOUT</a>
-                        <a>SERVICE</a>
-                        <a>MENU</a>
-                        <div>
-                            <a>Pages</a>
-                            <div className="d-none">
-                                <a>Booking</a>
-                                <a>Our Team</a>
-                                <a>Testimonial</a>
-                            </div>
-                        </div>
-                        <a>CONTACT</a>
+            <nav className="relative inline-block w-full align-top h-[91px] bg-transparent z-50">
+                <div className="relative px-[50px] mt-[30px] flex items-stretch justify-center">
+                    <div className="flex items-center justify-start w-full h-full">
+                        <ul className=" items-center h-full list-none">
+                            <li>
+                                <a>
+                                    <span>Home</span>
+                                </a>
+                             </li>
+                            <li>
+                                <a>
+                                    <span>Menu</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a>
+                                    <span>Pages</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a>
+                                    <span>Shop</span>
+                                </a>
+                            </li>
+                        </ul>
                     </div>
-                    <a>BOOK A TABLE</a>
+                    <a>
+                        <img src={Logo}/>
+                    </a>
+                    <div>
+                        <div>
+                            <a>
+                                <span>Book a Table</span>
+                            </a>
+                        </div>
+                    </div>
                 </div>
             </nav>
         </div>
